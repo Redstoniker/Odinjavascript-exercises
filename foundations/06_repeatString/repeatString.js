063771514e3c7e134571aa78f8652aa2ef27844a
Redstoniker <287007098+Redstoniker@users.noreply.github.com>
2026-05-26 23:23:00 +0200

    Solve foundations/06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if( num < 0 ) {
+        return `ERROR`;
+    }
+    let calcArr = [];
+    for(let i = 0; i < num; i++){
+        calcArr[i] = string;
+    }
+    return calcArr.join("");
 };
 
 // Do not edit below this line
