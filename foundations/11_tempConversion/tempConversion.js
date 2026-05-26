d7c8cdcdb81c8ba7f637c858085a949bcb723693
Redstoniker <287007098+Redstoniker@users.noreply.github.com>
2026-05-27 00:55:30 +0200

    Solve foundations/11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  let temperatureCelsius = (temperatureFahrenheit-32)*5/9;
+  temperatureCelsius = Math.round(temperatureCelsius*10)/10;
+  return(temperatureCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  let temperatureFahrenheit = (temperatureCelsius*9/5 +32);
+  temperatureFahrenheit = Math.round(temperatureFahrenheit*10)/10;
+  return temperatureFahrenheit;
 };
 
 // Do not edit below this line
