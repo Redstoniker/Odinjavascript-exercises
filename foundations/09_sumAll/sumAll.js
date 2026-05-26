95f3a9f15d104fd682fcab20bef1338ab5e5d078
Redstoniker <287007098+Redstoniker@users.noreply.github.com>
2026-05-27 00:38:50 +0200

    Solve foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+ // s < l  t = l-s+1
+ // s + s+1 + s+2 + s+3 ... (t times) = t*s + (1+2+3+...+t-1)
+ // 1+2+3+...+t-1 = 1+t-1 + 2+t-2 ... ((t-1)/2 times) 
+ // 1+t-1 + 2+t-2 ... = t + t + t
+ // therefore 1+2+3+...+t-1 = t*(t-1)/2
+ // therefore s + s+1 + s+2 + s+3 ... (t times) = t*s + (t-1)*t/2
+ // t*s + (t-1)*t/2 = (2s + (t-1))* t/2 = (2s+t-1)* t/2
+
+ return (isNaN(a) || isNaN(b) || a < 0 || b < 0 || Math.round(a) !== a || Math.round(b) !== b)
+    ? "ERROR"
+    : (a<b)
+        ? (b**2 + b - a**2 + a)/2
+        : (a**2 + a - b**2 + b)/2;
+ 
 
 };
 
