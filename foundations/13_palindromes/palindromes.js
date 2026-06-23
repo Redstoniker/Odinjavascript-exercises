70caba50a8bb8d1a97e4752932a863fba0c0e2a6
Redstoniker <287007098+Redstoniker@users.noreply.github.com>
2026-06-23 19:18:50 +0200

    Solve foundations/13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replaceAll(" ", "").replaceAll("!", "").replaceAll(",", "").replaceAll(".", "").split("");
+    palindrom = word.toReversed();
+    return word.toString()==palindrom.toString();
 };
 
 // Do not edit below this line
