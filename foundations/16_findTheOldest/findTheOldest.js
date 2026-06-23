a3750a6e21c981d96ce7005d8235a8c4a28a92a6
Redstoniker <287007098+Redstoniker@users.noreply.github.com>
2026-06-23 19:51:55 +0200

    Solve foundations/16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+const YEAR = Number(Date().split(" ")[3]);
+let ages = [];
+for(person of people){
+    if (person.yearOfDeath == undefined){
+        ages.push(YEAR - person.yearOfBirth);
+    }
+    else{
+        ages.push(person.yearOfDeath - person.yearOfBirth);
+    }
+}
+let maxAge = Math.max.apply(null, ages);
+let index = ages.indexOf(maxAge);
+return people[index];
 };
 
 // Do not edit below this line
